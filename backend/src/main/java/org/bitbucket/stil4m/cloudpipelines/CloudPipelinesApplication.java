c4530c100bde002d035337af2bbc56f18b0c07fd
Mats Stijlaart <mstijlaart@gmail.com>
2018-12-06 19:27:52 +0100

    Add backend and elm

---
+++ backend/src/main/java/org/bitbucket/stil4m/cloudpipelines/CloudPipelinesApplication.java
@@ -0,0 +1,12 @@
+package org.bitbucket.stil4m.cloudpipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPipelinesApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/org/bitbucket/stil4m/cloudpipelines/CloudPipelinesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.bitbucket.stil4m.cloudpipelines;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudPipelinesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
